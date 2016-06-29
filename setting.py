257f60bf537b1588433e0b4ccc6e6d79e4cc9631
m85091081 <b10216041@yuntech.edu.tw>
2016-06-29 10:26:59 +0800

    fix

---
+++ setting.py
@@ -0,0 +1,12 @@
+ALLPAY_SANDBOX = False
+AIO_SERVICE_URL = 'https://payment.allpay.com.tw/Cashier/AioCheckOut'
+AIO_SANDBOX_SERVICE_URL = 'http://payment-stage.allpay.com.tw/Cashier/AioCheckOut'
+IGNORE = 'Credit'
+
+MERCHANT_ID =''
+HASH_KEY = ''
+HASH_IV = ''
+
+RETURN_URL = ''
+CLIENT_BACK_URL = ''
+PAYMENT_INFO_URL = ''
